33fb9cc5f4b8f1e9175ae3a0e69d03b0eec66aa2
Mike Kelly <pioto@pioto.org>
2008-01-23 17:02:06 +0000

    Add a mutex to hopefully solve some race issues.
    git-svn-id: svn+ssh://svn.pioto.org/var/svn/rubeak/trunk@12 45f9f672-072c-0410-acbd-e5be69ac9e04

--- rubeak.rb
+++ rubeak.rb
@@ -27,8 +27,8 @@ $media_modes = [ 'mpd', 'lastfm' ]
 #
 # If you don't have this remote (chances are you don't), comment this line out, and uncomment the
 # other one:
-$hid_read_path="#{ENV['HOME']}/src/creative_rm1500_usb-0.1/hid_read"
-#$hid_read_path=nil
+#$hid_read_path="#{ENV['HOME']}/src/creative_rm1500_usb-0.1/hid_read"
+$hid_read_path=nil
 
 class Rubeak
 	def initialize
@@ -56,6 +56,8 @@ class Rubeak
 		@statusosd.outline_color='black'
 
 		@media_mode=$media_modes[0]
+
+		@mutex=Mutex.new
 	end
 
 	# Gets the current volume from alsa
@@ -125,119 +127,121 @@ class Rubeak
 
 	# Does magic stuff for the given action
 	def doaction (key)
-		case key
-		# Volume Control
-		when 'mute'
-			vm = getvol
-			if vm['mute']
-				system('amixer sset Master unmute &>/dev/null')
-			else
-				system('amixer sset Master mute &>/dev/null')
-			end
-			showvol
-		when 'vol+'
-			system("amixer sset Master 1+ &>/dev/null")
-			showvol
-		when 'vol-'
-			system("amixer sset Master 1- &>/dev/null")
-			showvol
-		# Media Player control
-		when 'play-pause', 'play', 'pause'
-			case @media_mode
-			when 'mpd'
-				mpc = IO::popen("mpc")
-				mpc.each do |x|
-					/^\[/.match(x) or next
-					if /\[playing\]/.match(x)
-						system("mpc pause &>/dev/null")
-						show_mpd
-						return
+		@mutex.synchronize do
+			case key
+			# Volume Control
+			when 'mute'
+				vm = getvol
+				if vm['mute']
+					system('amixer sset Master unmute &>/dev/null')
+				else
+					system('amixer sset Master mute &>/dev/null')
+				end
+				showvol
+			when 'vol+'
+				system("amixer sset Master 1+ &>/dev/null")
+				showvol
+			when 'vol-'
+				system("amixer sset Master 1- &>/dev/null")
+				showvol
+			# Media Player control
+			when 'play-pause', 'play', 'pause'
+				case @media_mode
+				when 'mpd'
+					mpc = IO::popen("mpc")
+					mpc.each do |x|
+						/^\[/.match(x) or next
+						if /\[playing\]/.match(x)
+							system("mpc pause &>/dev/null")
+							show_mpd
+							return
+						end
+					end
+					system("mpc play &>/dev/null")
+					show_mpd
+				when 'lastfm'
+					i=send_lastfm "info %u"
+					if i.nil?
+						last_track=""
+						File.open("#{ENV['HOME']}/.shell-fm/radio-history") \
+							{ |f| last_track=f.to_a[-1] }
+						send_lastfm "play lastfm://#{last_track}"
+						@statusosd.display_message(0,
+							"Playing last played track...")
+						@statusosd.timeout=5
+					else
+						send_lastfm "pause"
+						@statusosd.display_message(0,
+							"Pausing/resuming lastfm...")
+						@statusosd.timeout=5
 					end
 				end
-				system("mpc play &>/dev/null")
-				show_mpd
-			when 'lastfm'
-				i=send_lastfm "info %u"
-				if i.nil?
-					last_track=""
-					File.open("#{ENV['HOME']}/.shell-fm/radio-history") \
-						{ |f| last_track=f.to_a[-1] }
-					send_lastfm "play lastfm://#{last_track}"
-					@statusosd.display_message(0,
-						"Playing last played track...")
-					@statusosd.timeout=5
-				else
-					send_lastfm "pause"
-					@statusosd.display_message(0,
-						"Pausing/resuming lastfm...")
+			when 'rec', 'favorite'
+				case @media_mode
+				when 'lastfm'
+					send_lastfm "love"
+					@statusosd.display_message(0,"Current lastfm track loved.")
 					@statusosd.timeout=5
 				end
-			end
-		when 'rec', 'favorite'
-			case @media_mode
-			when 'lastfm'
-				send_lastfm "love"
-				@statusosd.display_message(0,"Current lastfm track loved.")
-				@statusosd.timeout=5
-			end
-		when 'prev'
-			case @media_mode
-			when 'mpd'
-				system("mpc prev &>/dev/null")
-				show_mpd
-			when 'lastfm'
-			end
-		when 'next'
-			case @media_mode
-			when 'mpd'
-				system("mpc next &>/dev/null")
-				show_mpd
-			when 'lastfm'
-				send_lastfm "skip"
-				@statusosd.display_message(0,"Skipping current lastfm track...")
-				@statusosd.timeout=5
-			end
-		when 'stop-eject', 'stop'
-			case @media_mode
-			when 'mpd'
-				system("mpc stop &>/dev/null")
-				show_mpd
-			when 'lastfm'
-				send_lastfm "stop"
-				@statusosd.display_message(0,"Stopping lastfm play...")
-				@statusosd.timeout=5
-			end
-		# Misc
-		when 'power'
-			xset = IO::popen("xset -q")
-			xset.each do |x|
-				/Monitor/.match(x) or next
-				if /Off/.match(x)
-					system("xset dpms force on &>/dev/null")
-				else
-					system("xset dpms force off &>/dev/null")
+			when 'prev'
+				case @media_mode
+				when 'mpd'
+					system("mpc prev &>/dev/null")
+					show_mpd
+				when 'lastfm'
 				end
+			when 'next'
+				case @media_mode
+				when 'mpd'
+					system("mpc next &>/dev/null")
+					show_mpd
+				when 'lastfm'
+					send_lastfm "skip"
+					@statusosd.display_message(0,"Skipping current lastfm track...")
+					@statusosd.timeout=5
+				end
+			when 'stop-eject', 'stop'
+				case @media_mode
+				when 'mpd'
+					system("mpc stop &>/dev/null")
+					show_mpd
+				when 'lastfm'
+					send_lastfm "stop"
+					@statusosd.display_message(0,"Stopping lastfm play...")
+					@statusosd.timeout=5
+				end
+			# Misc
+			when 'power'
+				xset = IO::popen("xset -q")
+				xset.each do |x|
+					/Monitor/.match(x) or next
+					if /Off/.match(x)
+						system("xset dpms force on &>/dev/null")
+					else
+						system("xset dpms force off &>/dev/null")
+					end
+				end
+			when 'display'
+				showvol
+				@statusosd.display_message(0,"Current Time: #{`date`.chomp}")
+				@statusosd.timeout=5
+				case @media_mode
+				when 'mpd'
+					show_mpd
+				when 'lastfm'
+					show_lastfm
+				end
+			when 'options', 'mode'
+				mode_index = $media_modes.index(@media_mode)+1
+				if mode_index >= $media_modes.size
+					mode_index=0
+				end
+				@media_mode=$media_modes[mode_index]
+				@statusosd.display_message(0,"Media mode now: #@media_mode")
+				@statusosd.timeout=5
+			else
+				puts "!!! Unknown action: '#{key}'"
 			end
-		when 'display'
-			showvol
-			@statusosd.display_message(0,"Current Time: #{`date`.chomp}")
-			@statusosd.timeout=5
-			case @media_mode
-			when 'mpd'
-				show_mpd
-			when 'lastfm'
-				show_lastfm
-			end
-		when 'options', 'mode'
-			mode_index = $media_modes.index(@media_mode)+1
-			if mode_index >= $media_modes.size
-				mode_index=0
-			end
-			@media_mode=$media_modes[mode_index]
-			@statusosd.display_message(0,"Media mode now: #@media_mode")
-			@statusosd.timeout=5
-		else
-			puts "!!! Unknown action: '#{key}'"
 		end
 	end
 
